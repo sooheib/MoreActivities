de285d0dbf04ff01552e7410a09e695e7eeeeb73
sooheib <soosel93@gmail.com>
2016-06-01 16:50:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sooheib/moreactivities/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sooheib.moreactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToSecond(View view) {
+        Intent intent=new Intent(this,SecondActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/sooheib/moreactivities/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.sooheib.moreactivities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sooheib on 6/1/16.
+ */
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
