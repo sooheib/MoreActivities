adb37102e271166a372c9015908e557cb5bc4f37
sooheib <soosel93@gmail.com>
2016-06-01 17:14:08 +0100

    Passing data from activity to another

--- app/src/main/java/com/example/sooheib/moreactivities/MainActivity.java
+++ app/src/main/java/com/example/sooheib/moreactivities/MainActivity.java
@@ -4,17 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView dataBackTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText=(EditText) findViewById(R.id.editText);
+        dataBackTextView= (TextView) findViewById(R.id.dataBackTextView);
+
     }
 
     public void goToSecond(View view) {
-        Intent intent=new Intent(this,SecondActivity.class);
-        startActivity(intent);
+        String str=editText.getText().toString();
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("username",str);
+        //startActivity(intent);
+        startActivityForResult(intent, 123);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==123){
+            dataBackTextView.setText(data.getStringExtra("databack"));
+        }
     }
 }
--- app/src/main/java/com/example/sooheib/moreactivities/SecondActivity.java
+++ app/src/main/java/com/example/sooheib/moreactivities/SecondActivity.java
@@ -1,16 +1,36 @@
 package com.example.sooheib.moreactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by sooheib on 6/1/16.
  */
 public class SecondActivity extends AppCompatActivity {
 
+    TextView textView;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        textView= (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText2);
+        String str =getIntent().getStringExtra("username");
+        textView.setText("Welcome, " + str);
+
+    }
+
+    public void sendDataBack(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("databack", editText.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+
+
     }
 }
